526cb1ef9efae55edc551f8ae5484aff675f2701
selenaou <56272881+selenaou@users.noreply.github.com>
2019-10-28 15:13:09 -0400

    More about Selena fixed

--- calcSelena.js
+++ calcSelena.js
@@ -1,8 +1,8 @@
-function calcBMI(){
-  r = radius.value  // read weight from element with id="weight"
+function calcVolumes(){
+  r = radius.value  // read radius from element with id="radius"
   h = height.value  // read height from element with id="height"
-  volume = r*r*h*Math.PI  // calculate the Body Mass Index
-  Volume.innerHTML =    // write into label element with id "BMI"
+  volume = r*r*h*Math.PI  // calculate the Volume
+  Volume.innerHTML =    // write into label element with id "Volume"
       "The Volume of the can is "
-      +volume.toFixed(1)  // round BMI to 1 decimal place
+      +volume.toFixed(1)  // round Volume to 1 decimal place
 }
