0e6a311ad1ca1bf77099a792157114578fa138b0
selenaou <56272881+selenaou@users.noreply.github.com>
2019-10-30 14:27:28 -0400

    About Selena Fixed

--- calcSelena.js
+++ calcSelena.js
@@ -2,6 +2,8 @@ function calcVolumes(){
   r = radius.value  // read radius from element with id="radius"
   h = height.value  // read height from element with id="height"
   volume = r*r*h*Math.PI  // calculate the Volume
+  console.log("hello world")
+  console.log(volume)
   Volume.innerHTML =    // write into label element with id "Volume"
       "The Volume of the can is "
       +volume.toFixed(1)  // round Volume to 1 decimal place
