59dcee498ecb8b405814a615a8dd9c72ee13f127
Nicholas62301 <nicholas.k.ong623@gmail.com>
2019-10-30 15:08:29 -0400

    update

--- CalcNIcholas.js
+++ CalcNIcholas.js
@@ -5,10 +5,11 @@ function CalcArea(){
   h = height.value  // read height from element with id="height"
 
   w = width.value   // read width from element with id= "width"
-  bmi = 703*l*w*h  // calculate the Body Mass Index
 
-  area.innerHTML =    // write into label element with id "BMI"
+  volume = (l*w*h)  // calculate the volume of the cube
+
+  volume.innerHTML =    // write into label element with id "BMI"
 
       "The volume of the cube is... "
 
-      +area.toFixed(1)  // round area to 1 decimal place
+      +volume.toFixed(1)  // round area to 1 decimal place
